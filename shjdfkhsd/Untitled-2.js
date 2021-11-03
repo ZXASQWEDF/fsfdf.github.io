29aec3c81c8cef91316fbebb48dc1bdd7fb12d7b
jackyhoi <87819234+jackyhoi@users.noreply.github.com>
2021-11-03 23:22:53 +0800

    Add files via upload

--- shjdfkhsd/Untitled-2.js
+++ shjdfkhsd/Untitled-2.js
@@ -18,7 +18,7 @@ function login() {
         if (accountValue === 'hoikahei' &&
             passwordValue === '070608') {
             resultOutput.style.color = '#0f0'
-            window.location.href = 'https://sites.google.com/view/dfgdfddddwqq/%E4%BB%A3%E6%89%93%E7%B3%BB%E7%B5%B1';
+            window.location.href = 'https://sites.google.com/view/hoikahei/%E4%BB%A3%E6%89%93%E7%B3%BB%E7%B5%B1';
         } else {
             resultOutput.style.color = '#f00'
             resultOutput.innerText = '請用戶帳號是否正確'
