843f9451731d118b1c08af047abc1132798a8ffe
jackyhoi <87819234+jackyhoi@users.noreply.github.com>
2021-11-08 22:36:37 +0800

    Add files via upload

--- shjdfkhsd/const loginButton = document.getElementB.js
+++ shjdfkhsd/const loginButton = document.getElementB.js
@@ -1,5 +1,4 @@
 const loginButton = document.getElementById('login')
-const accountInput = document.getElementById('account')
 const passwordInput = document.getElementById('password')
 const resultOutput = document.getElementById('result')
 
@@ -7,17 +6,13 @@ function login() {
     resultOutput.style.color = '#00f'
     resultOutput.innerText = 'Login..'
     setTimeout(function() {
-        const accountValue = accountInput.value
-        passwordValue = passwordInput.value
-        if (accountValue === 'GORDAN' &&
+        const passwordInput = accountInput.value
+
+        if (
             passwordValue === '664089') {
             resultOutput.style.color = '#0f0'
             window.location.href = 'https://jackyhoi.github.io/fsfdf.github.io/shjdfkhsd/ppp.html';
-        } else
-        if (accountValue === 'hoikahei' &&
-            passwordValue === '070608') {
-            resultOutput.style.color = '#0f0'
-            window.location.href = 'https://sites.google.com/view/hoikahei/%E4%BB%A3%E6%89%93%E7%B3%BB%E7%B5%B1';
+
         } else {
             resultOutput.style.color = '#f00'
             resultOutput.innerText = '帳號或密碼不正確'
